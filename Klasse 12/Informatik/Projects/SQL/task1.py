65c7dbd38cce83ee21fe2a7fd582482a422bfaab
Karl Zschiebsch <karl.zschiebsch@gmail.com>
2023-11-16 12:40:24 +0100

    Linux Mint Backup

--- Klasse 12/Informatik/Projects/SQL/task1.py
+++ Klasse 12/Informatik/Projects/SQL/task1.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+connection = sqlite3.connect('project.db')
+
+cursor = connection.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS Kurs(ID, Kursname);')
+
+print(cursor.execute('.tables'))
